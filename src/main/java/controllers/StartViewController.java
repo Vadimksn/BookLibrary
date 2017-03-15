0886d8b6cc625a4c9c45c0c68d875803c5ec7141
Vadym Kasyniuk <vadimksn@gmail.com>
2017-03-15 23:47:07 +0200

    added StartViewController

---
+++ src/main/java/controllers/StartViewController.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import controllers.book.tabs.BooksAvailableController;
+import controllers.book.tabs.BooksController;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Vadim on 15.03.2017.
+ */
+public class StartViewController implements Initializable{
+    @FXML
+    private Tab tabBooksAvailable,tabBooksNotAvailable,tabBooks,tabStudents,tabStudentsBlackList;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setTabBooksAvailableListener();
+        setTabBooksListener();
+    }
+
+    private void setTabBooksAvailableListener(){
+        tabBooksAvailable.setOnSelectionChanged(event ->{
+            BooksAvailableController.getInstance().initTableData();
+        });
+    }
+
+    private void setTabBooksListener(){
+        tabBooks.setOnSelectionChanged(event ->{
+            BooksController.getInstance().initTableData();
+        });
+    }
+}
